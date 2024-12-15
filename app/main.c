1f18d027ee0a8d3e6dc3e215496d3e082bc15962
ale_carella <useradd@DESKTOP-U4GSC2L>
2024-12-15 18:09:05 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -18,6 +18,20 @@ int main() {
 			printf("%s\n", input + strlen("echo") + 1);
 			continue;
 		}
+
+		if (strncmp(input, "type", strlen("type")) == 0) {
+			if(strncmp(input + strlen("type") + 1, "echo", strlen("echo")) == 0||
+				strncmp(input + strlen("type") + 1, "type", strlen("type")) == 0||
+				strncmp(input + strlen("type") + 1, "exit", strlen("exit")) == 0)
+			{
+				printf("%s is a shell builtin\n", input + strlen("type") + 1);
+				continue;
+			} else {
+				printf("%s: not found\n", input + strlen("type") + 1);
+				continue;
+			}
+		}
+
 		printf("%s: command not found\n", input);
 		fflush(stdin);
 	}
