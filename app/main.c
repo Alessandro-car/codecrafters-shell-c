15e9939e440eccd5930f48e63644559ca3f63432
ale_carella <useradd@DESKTOP-U4GSC2L>
2024-12-15 17:55:16 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -3,23 +3,23 @@
 #include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
 
   // Wait for user input
   while(1) {
+		printf("$ ");
+		fflush(stdout);
 		char input[100];
   	fgets(input, 100, stdin);
   	input[strlen(input) - 1] = '\0';
 		if (strcmp(input, "exit 0") == 0) {
-			exit(0);
-		} else {
-			printf("%s: command not found\n", input);
-			printf("$ ");
-			fflush(stdout);
-			fflush(stdin);
+				exit(0);
 		}
+		if (strncmp(input, "echo", strlen("echo")) == 0) {
+			printf("%s\n", input + strlen("echo") + 1);
+			continue;
+		}
+		printf("%s: command not found\n", input);
+		fflush(stdin);
 	}
 	return 0;
 }
